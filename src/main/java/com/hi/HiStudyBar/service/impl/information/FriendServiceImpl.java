f1a7e320921fc70883c3308d675f0ee90571ced9
TowerLoong <towerloong@hotmail.com>
2018-01-28 18:21:03 +0800

    添加了service层中和friend有关的接口和实现类

---
+++ src/main/java/com/hi/HiStudyBar/service/impl/information/FriendServiceImpl.java
@@ -0,0 +1,10 @@
+package com.hi.HiStudyBar.service.impl.information;
+
+import org.springframework.stereotype.Service;
+
+import com.hi.HiStudyBar.service.intf.information.IFriendService;
+
+@Service
+public class FriendServiceImpl implements IFriendService{
+
+}
